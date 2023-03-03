d641b454d82467175b7dbec95d7444bcaaa54a88
HedvikaCapkova <hedvika.capkova@gmail.com>
2023-03-03 21:23:45 +0100

    lesson 5

---
+++ 230302_lekce_5/Cviceni-React-udalosti/src/Uloha5/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+// Zadání 1: Dopiš komponentu `NakupniPolozka`, aby v divu byl vypsaný název položky a tlačítko s textem „koupit“.
+// Zadání 2: Po kliknutí na tlačítko vypiš do konzole „Uživatel chce koupit DOPLŇ_NÁZEV_POLOŽKY“.
+
+export const NakupniPolozka = ({ nazev }) => {
+	return (
+		<>
+			<div>
+				{nazev} 
+				<button onClick={() => {console.log(`Uživatel chce koupit ${nazev}`)}}>koupit</button>
+			</div>
+		</>
+	)
+}
+
+// Zadání 3: Pomocí `polozky.map` vypiš všechny názvy jako nákupní položky.
+
+// Bonus: Nastav jednotlivým položkám ve výpisu `key`, aby vývojářské nástroje v prohlížeči nezobrazovaly varování. Nápověda: Key musí být v seznamu unikátní. Použij název položky.
+
+export const Uloha5 = () => {
+	const polozky = ['hrušky', 'jablka', 'třešně', 'jahody']
+
+	return (
+		<>
+			<NakupniPolozka key="1" nazev="hrušky" />
+		</>
+	)
+}
