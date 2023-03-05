32d838a9cea70bf7fef91c888fc8e96ed84ee421
HedvikaCapkova <hedvika.capkova@gmail.com>
2023-03-05 22:00:58 +0100

    lekce 5 - uloha 4

--- 230302_lekce_5/Cviceni-React-stav/src/Uloha2/index.jsx
+++ 230302_lekce_5/Cviceni-React-stav/src/Uloha2/index.jsx
@@ -4,17 +4,16 @@ import React, { useState } from 'react'
 // Zadání 2: Nastav, aby jednotlivá tlačítka měnila stav. +1 přičítalo jedničku, +5 přičítalo pětku, vynulovat nastavovalo na nulu apod.
 
 const Pocitadlo = () => {
-	let cislo = 0
-	// const [cislo, setcislo] = useState(0); 
+	const [cislo, setcislo] = useState(0)
 
 	return (
 		<>
 			<h3>Počítadlo: {cislo}</h3>
-			<button onClick={() => cislo = cislo-5}>-5</button>
-			<button onClick={() => cislo = cislo-1}>-1</button>
-			<button onClick={() => console.log(cislo)}>vynulovat</button>
-			<button onClick={() => console.log(cislo)}>+1</button>
-			<button onClick={() => console.log(cislo)}>+5</button>
+			<button onClick={() => setcislo(cislo-5)}>-5</button>
+			<button onClick={() => setcislo(cislo-1)}>-1</button>
+			<button onClick={() => setcislo(0)}>vynulovat</button>
+			<button onClick={() => setcislo(cislo+1)}>+1</button>
+			<button onClick={() => setcislo(cislo+5)}>+5</button>
 		</>
 	)
 }
