eab42705009c9576c3c33decdbd53d50ad547517
HedvikaCapkova <hedvika.capkova@gmail.com>
2023-03-07 18:41:50 +0100

    stav - uloha 7

--- 230302_lekce_5/Cviceni-React-stav/src/Uloha7/index.jsx
+++ 230302_lekce_5/Cviceni-React-stav/src/Uloha7/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './ukazatel-uspechu.css'
+import React, { useState } from 'react';
+import './ukazatel-uspechu.css';
 
 // Zadání 1: Použij prop `barva` ve style atributu.
 // Zadání 2: Přidej komponentě číselný stav od nuly do sta, který bude reprezentovat postup.
@@ -7,28 +7,42 @@ import './ukazatel-uspechu.css'
 // Zadání 4. Nastav prvku `ukazatel-uspechu__postup` šířku podle stavové proměnné.
 
 const UkazatelPokroku = ({ barva }) => {
-	return (
-		<div className="ukazatel-uspechu">
-			<div className="ukazatel-uspechu__ramecek">
-				<div
-					className="ukazatel-uspechu__postup"
-					style={{
-						width: '40%',
-						backgroundColor: 'red',
-					}}
-				></div>
-			</div>
-			<button>postoupit o 10 %</button>
-		</div>
-	)
-}
+  const [postup, setPostup] = useState(0);
+  return (
+    <div className="ukazatel-uspechu">
+      <div className="ukazatel-uspechu__ramecek">
+        <div
+          className="ukazatel-uspechu__postup"
+          style={{
+            width: `${postup}%`,
+            backgroundColor: barva,
+          }}></div>
+      </div>
+      <button
+        onClick={() => {
+          postup < 100 && setPostup(postup + 10);
+        }}>
+        postoupit o 10 %
+      </button>
+    </div>
+  );
+};
 
 export const Uloha7 = () => {
-	return (
-		<>
-			<UkazatelPokroku barva="blue" />
-			<UkazatelPokroku barva="green" />
-			<UkazatelPokroku barva="purple" />
-		</>
-	)
-}
+  return (
+    <>
+      <UkazatelPokroku
+        key="1"
+        barva="blue"
+      />
+      <UkazatelPokroku
+        key="2"
+        barva="green"
+      />
+      <UkazatelPokroku
+        key="3"
+        barva="purple"
+      />
+    </>
+  );
+};
