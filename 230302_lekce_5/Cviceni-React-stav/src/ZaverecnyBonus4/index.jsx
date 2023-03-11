3121fa77958588a63b83f4e1584eb9484a9a55eb
HedvikaCapkova <hedvika.capkova@gmail.com>
2023-03-11 19:46:11 +0100

    stav - bonus 4

--- 230302_lekce_5/Cviceni-React-stav/src/ZaverecnyBonus4/index.jsx
+++ 230302_lekce_5/Cviceni-React-stav/src/ZaverecnyBonus4/index.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 // Zadání: Pomocí dvou stavových proměnných s výchozí hodnotou 0, které se budou měnit podle vstupních políček, vypiš do `<output></output>` jejich součet.
 
 export const ZaverecnyBonus4 = () => {
-	return (
-		<>
-			<input type="number" defaultValue="0" /> +{' '}
-			<input type="number" defaultValue="0" /> = <output>0</output>
-		</>
-	)
-}
+  const [firstInput, setFirstInput] = useState(0);
+  const [secondInput, setSecondInput] = useState(0);
+  return (
+    <>
+      <input
+        type="number"
+        value={firstInput}
+        onChange={(e) => setFirstInput(e.target.value)}
+      />{' '}
+      +{' '}
+      <input
+        type="number"
+        value={secondInput}
+        onChange={(e) => setSecondInput(e.target.value)}
+      />{' '}
+      = <output>{Number(firstInput) + Number(secondInput)}</output>
+    </>
+  );
+};
