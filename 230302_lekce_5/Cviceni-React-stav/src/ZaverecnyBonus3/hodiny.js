8a131c5dc57a8f0315e17655df30f8a5b01613c0
HedvikaCapkova <hedvika.capkova@gmail.com>
2023-03-11 19:36:11 +0100

    stav - bonus 3

--- 230302_lekce_5/Cviceni-React-stav/src/ZaverecnyBonus3/hodiny.js
+++ 230302_lekce_5/Cviceni-React-stav/src/ZaverecnyBonus3/hodiny.js
@@ -1,22 +1,22 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
 const ziskejSekundy = () => {
-	const ted = new Date()
-	return ted.getSeconds() + ted.getMinutes() * 60 + ted.getHours() * 60 * 60
-}
+  const ted = new Date();
+  return ted.getSeconds() + ted.getMinutes() * 60 + ted.getHours() * 60 * 60;
+};
 
 export const useCasVSekundach = () => {
-	const [sekundy, setSekundy] = useState(ziskejSekundy())
+  const [sekundy, setSekundy] = useState(ziskejSekundy());
 
-	useEffect(() => {
-		const odpocet = setTimeout(() => {
-			setSekundy(ziskejSekundy())
-		}, 1000 - new Date().getMilliseconds())
+  useEffect(() => {
+    const odpocet = setTimeout(() => {
+      setSekundy(ziskejSekundy());
+    }, 1000 - new Date().getMilliseconds());
 
-		return () => {
-			clearTimeout(odpocet)
-		}
-	}, [sekundy])
+    return () => {
+      clearTimeout(odpocet);
+    };
+  }, [sekundy]);
 
-	return sekundy
-}
+  return sekundy;
+};
--- 230302_lekce_5/Cviceni-React-stav/src/ZaverecnyBonus3/index.jsx
+++ 230302_lekce_5/Cviceni-React-stav/src/ZaverecnyBonus3/index.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import { useCasVSekundach } from './hodiny'
+import React, { useState } from 'react';
+import { useCasVSekundach } from './hodiny';
 
 // Zadání: Pomocí předchystané stavové proměnné, která se mění každou sekundu, zobraz aktuální čas ve formátu 7:49:05.
 
 // Nápověda: Pokud je v `casVSekundach` nula, pak je 0:00:00. Pokud je v `casVSekundach` 60, pak je 0:01:00. Pokud je v `casVSekundach` 67, pak je 0:01:07.
 
 export const ZaverecnyBonus3 = () => {
-	const casVSekundach = useCasVSekundach()
+  const casVSekundach = useCasVSekundach();
 
-	return (
-		<>
-			Čas právě teď: <b>{casVSekundach}</b>
-		</>
-	)
-}
+  let hours = Math.floor(casVSekundach / (60 * 60));
+  let minutes = Math.floor((casVSekundach % (60 * 60)) / 60)
+    .toString()
+    .padStart(2, 0);
+  let seconds = Math.ceil((casVSekundach % (60 * 60)) % 60)
+    .toString()
+    .padStart(2, 0);
+
+  return (
+    <>
+      Čas právě teď: <b>{`${hours}:${minutes}:${seconds}`}</b>
+    </>
+  );
+};
