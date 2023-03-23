b5a4b907a3271f2c265c1962224d3cc1df4c8426
HedvikaCapkova <hedvika.capkova@gmail.com>
2023-03-23 20:47:32 +0100

    lekce 8 - komunikace mezi sourozenci - hotovo

---
+++ 230323_lekce_8/dotaznik/src/Option/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Icon from '../Icon/index';
+import './style.css';
+
+const Option = ({ type, text, onSelected }) => {
+  const handleClick = () => {
+    onSelected(type);
+  };
+  return (
+    <div
+      className="option"
+      onClick={handleClick}>
+      <Icon type={type} />
+      <div>{text}</div>
+    </div>
+  );
+};
+
+export default Option;
