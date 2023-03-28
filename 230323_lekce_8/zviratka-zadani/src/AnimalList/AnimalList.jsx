8e2dc725aac24ce245be2a5c8e084f6cbca74ff3
HedvikaCapkova <hedvika.capkova@gmail.com>
2023-03-28 22:22:45 +0200

    created components

---
+++ 230323_lekce_8/zviratka-zadani/src/AnimalList/AnimalList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './AnimalList.css';
+
+export const AnimalList = ({}) => {
+  return (
+    <div class="animal-list">
+      <Animal />
+    </div>
+  );
+};
--- 230323_lekce_8/zviratka-zadani/src/index.jsx
+++ 230323_lekce_8/zviratka-zadani/src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { AnimalDetail } from './AnimalDetail/AnimalDetail';
+import { AnimalList } from './AnimalList/AnimalList';
 import './style.css';
 
 const App = () => {
@@ -8,12 +10,12 @@ const App = () => {
       <h1>Zvířátka v ZOO</h1>
 
       <div className="container">
+        <AnimalList />
+        <AnimalDetail />
         {/* zde budou jednotlivé komponenty */}
       </div>
     </>
   );
 };
 
-createRoot(
-  document.querySelector('#app'),
-).render(<App />);
+createRoot(document.querySelector('#app')).render(<App />);
